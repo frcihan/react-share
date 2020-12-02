f260d2be9d51b98fa3ff134bf732d0a355318cc6
ferruhcihan <ferruhcihan20@gmail.com>
2020-12-02 19:38:44 +0300

    centext added

---
+++ src/context/AuthContext.js
@@ -0,0 +1,26 @@
+import { createContext, useState, useEffect } from 'react'
+import firebase from "../firebase/firebase.utils"
+
+export const FirebaseAuthContext = createContext();
+
+function AuthContextProvider(props) {
+
+    const [isAuthenticated, setAuthenticated] = useState(false);
+    const [currentUser, setCurrentUser] = useState();
+
+    useEffect(() => {
+        firebase.firebaseAuth.onAuthStateChanged((user) => {
+            console.log("user", user);
+            setCurrentUser(user);
+        })
+
+    }, [])
+
+    return (
+        <FirebaseAuthContext.Provider value={{currentUser}}>
+            {props.children}
+        </FirebaseAuthContext.Provider>
+    )
+}
+
+export default AuthContextProvider;
