34ee0b1ba37b06f355cd94bad81e115551218cc2
ferruhcihan <ferruhcihan20@gmail.com>
2020-12-03 23:41:17 +0300

    router updated

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,8 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
+import Main from "../pages/Main"
 import Signup from "../pages/Signup"
 import Signin from "../pages/Signin"
+import UserDetail from "../pages/UserDetail"
 import Navbar from "../components/Navbar"
+import Footer from "../components/Footer";
 
 function AppRouter() {
     return (
@@ -11,7 +14,10 @@ function AppRouter() {
             <Switch>
                 <Route path="/register" component={Signup} />
                 <Route path="/login" component={Signin} />
+                <Route path="/user/:id" component={UserDetail} />
+                <Route path="/" component={Main} />
             </Switch>
+            <Footer />
         </Router>
     )
 }
